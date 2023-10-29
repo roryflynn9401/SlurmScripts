001412c11260e0c2af1a2400353e12305e86bd15
roryflynn9 <roryflynn.pc@hotmail.com>
2023-10-29 11:42:10 +0000

    Create dir and take copy of needed files for parallel testing - use with slurmJobs

--- runProc.sh
+++ runProc.sh
@@ -18,6 +18,10 @@ iterations="$3"
 source_file="$fileName.c"
 output_binary="$fileName"
 
+mkdir benchmark"$cpu_cores"
+cp process-a.c rawimage.h input-large.raw search-1.raw benchmark"$cpu_cores"
+cd benchmark"$cpu_cores"
+
 # Compile the program with -fopenmp and -std=c99
 gcc -o "$output_binary" -fopenmp -std=c99 "$source_file"
 
